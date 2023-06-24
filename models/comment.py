a3b4288bce49e78dc3484beb070a861a3b0bcbe4
StevieG46 <steve.w.gow@gmail.com>
2023-06-24 11:59:23 +1000

    Create Comment Model and relate it to User and Card

---
+++ models/comment.py
@@ -0,0 +1,25 @@
+from init import db, ma
+from marshmallow import fields
+
+class Comment(db.Model): # {id: 1, "message": "Comment 1", "user": {id: 1, name: "User 1"}}
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Columnn(db.Text)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='comments') # {id: 1, name: "User 1"}
+    card = db.relationship('Card', back_populates='comments')
+
+class CommentSchema(ma.Schema):
+    user = fields.Nested('UserSchema', only=['name', 'email']) 
+    card = fields.Nested('CardSchema', exclude=['comments'])
+
+    class Meta:
+        fields = ('id', 'message', 'card', 'user')
+        orderd = True
+
+comment_schema = CommentSchema()
+comments_schema = CommentSchema(many=True)
